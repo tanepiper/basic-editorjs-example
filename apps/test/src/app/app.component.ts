0f7e6ead6be5fb192c500cdc9013078b01627ea7
Tane Piper <tane-piper@idexx.com>
2020-12-04 09:11:45 +0100

    initial commit

---
+++ apps/test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-editor-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test';
+}
