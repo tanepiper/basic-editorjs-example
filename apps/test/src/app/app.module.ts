b2429e2eabbc9413e6e0d5ee8a68819d51fd9fbf
Tane Piper <tane-piper@idexx.com>
2020-12-04 09:21:41 +0100

    Works fine

--- apps/test/src/app/app.module.ts
+++ apps/test/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxEditorJSComponentModule } from '@tinynodes/ngx-editorjs';
+import { NgxEditorjsPluginsModule } from '@tinynodes/ngx-editorjs-plugins';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgxEditorJSComponentModule, NgxEditorjsPluginsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
